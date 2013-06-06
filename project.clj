219a76ee2f8aa4af882fb6169944db7db54627c3
SnOoPy <snoopy323@gmail.com>
2013-06-06 21:03:32 +0200

    Add public repo for mesos-0.11.0 and storm 0.8.2

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
-(defproject storm/storm-mesos "0.0.1-SNAPSHOT"
-  :description "Storm integration with the Mesos cluster manager"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
-                 "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"}
-  :dependencies [
-[org.apache.mesos/mesos "0.9.0-120"] ;; TODO: needs to be made public
-]
-  :dev-dependencies [
-[storm "0.9.0-wip15"]
-[org.clojure/clojure "1.4.0"]
-])
+(defproject storm/storm-mesos "0.8.2-SNAPSHOT"
+   :description "Storm integration with the Mesos cluster manager"
+   :source-path "src/clj"
+   :java-source-path "src/jvm"
+   :javac-options {:debug "true" :fork "true"}
+   :repositories {"releases" "http://repo1.maven.org/maven2"
+                  "snapshots" "http://repo1.maven.org/maven2"}
+   :dependencies [
+   [org.apache.mesos/mesos "0.11.0-incubating"]
+   [storm/storm "0.8.2"]
+ ]
+
+   :dev-dependencies [
+   [storm "0.9.0-wip15"]
+   [org.clojure/clojure "1.4.0"]
+ ])
