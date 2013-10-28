c3ee8d1bef7d2a29a465dbbcb82244183261df2b
Erich Nachbar <erich@stuffylab.com>
2013-10-28 10:51:32 -0700

    changing to storm 0.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-mesos "0.8.2-SNAPSHOT"
+(defproject storm/storm-mesos "0.9.0-SNAPSHOT"
    :description "Storm integration with the Mesos cluster manager"
    :source-path "src/clj"
    :java-source-path "src/jvm"
@@ -7,10 +7,10 @@
                   "snapshots" "http://repo1.maven.org/maven2"}
    :dependencies [
    [org.apache.mesos/mesos "0.11.0-incubating"]
-   [storm/storm "0.8.2"]
+   [storm/storm "0.9.0-SNAPSHOT"]
  ]
 
    :dev-dependencies [
-   [storm "0.9.0-wip15"]
+   [storm "0.9.0-SNAPSHOT"]
    [org.clojure/clojure "1.4.0"]
  ])
